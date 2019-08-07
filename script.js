350d4578246949667ac5f475392ce43e6d1e361e
pwilson77 <p.o.yirenkyi@gmail.com>
2019-08-07 12:48:00 +0000

    styled the form for inserting new books

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 
-  this.info = function() {
-    return `The ${title} by ${author}, ${pages} pages, ${read? "already read this" : "not read yet" }`
+  this.info = function () {
+    return `The ${title} by ${author}, ${pages} pages, ${read ? "already read this" : "not read yet"}`
   }
 }
 
-let book1  = new Book("Hobbit", "J.R.R. Tolkien", 295, false);
+let book1 = new Book("Hobbit", "J.R.R. Tolkien", 295, false);
 myLibrary.push(book1);
 
 function toogleForm() {
 
 }
 
-function addBookToLibrary(){
+function addBookToLibrary() {
   let addBook = document.getElementById('addBook')
   addBook.addEventListener('click', () => {
     const form = document.createElement('form');
@@ -26,11 +26,21 @@ function addBookToLibrary(){
   })
 }
 
-function render () {
-  
+function render() {
+  x = document.getElementById("form-parent");
+  x.style.display = "none";
   myLibrary.forEach((book) => {
     console.log(book.info());
   })
+}
+
+function displayForm() {
+  var x = document.getElementById("form-parent");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
